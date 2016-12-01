b5f88ac45800a9237b7ace9f81fe9b5548984f25
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-01 13:40:20 +0530

    added helium structure

--- SimpleDRClientTest/inscount.c
+++ SimpleDRClientTest/inscount.c
@@ -52,6 +52,11 @@
 #include "drmgr.h"
 #include <string.h>
 
+// Helium Specific
+//#include "dr_defines.h"
+#include "defines.h"
+//#include "dr_ir_instr.h"
+
 #ifdef WINDOWS
 # define DISPLAY_STRING(msg) dr_messagebox(msg)
 #else
@@ -79,6 +84,44 @@ static dr_emit_flags_t event_app_instruction(void *drcontext, void *tag,
 	instrlist_t *bb, instr_t *inst,
 	bool for_trace, bool translating,
 	void *user_data);
+
+// Integrating Helium clients into the simple client
+#define ARGUMENT_LENGTH 20
+
+typedef void(*thread_func_t) (void * drcontext);
+typedef void(*init_func_t) (client_id_t id, const char * name, const char * arguments);
+typedef void(*exit_func_t) (void);
+typedef void(*module_load_t) (void * drcontext, const module_data_t * info, bool loaded);
+typedef void(*module_unload_t) (void * drcontext, const module_data_t * info);
+
+typedef struct _cmdarguments_t {
+
+	char name[MAX_STRING_LENGTH];
+	char arguments[10 * MAX_STRING_LENGTH];
+
+} cmdarguments_t;
+
+typedef struct _instrumentation_pass_t {
+
+	char * name;
+	init_func_t init_func;
+	drmgr_analysis_cb_t analysis_bb;
+	drmgr_insertion_cb_t instrumentation_bb;
+	drmgr_xform_cb_t app2app_bb;
+	drmgr_priority_t priority;
+	thread_func_t thread_init;
+	thread_func_t thread_exit;
+	exit_func_t process_exit;
+	module_load_t module_load;
+	module_unload_t module_unload;
+
+
+} instrumentation_pass_t;
+
+//allocate statically enough space
+static cmdarguments_t arguments[ARGUMENT_LENGTH];
+static instrumentation_pass_t ins_pass[ARGUMENT_LENGTH];
+static int argument_length = 0;
 
 DR_EXPORT void
 dr_client_main(client_id_t id, int argc, const char *argv[])
@@ -128,22 +171,6 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
 		dr_fprintf(STDERR, "Client inscount is running\n");
 	}
 #endif
-}
-
-static void
-event_exit(void)
-{
-#ifdef SHOW_RESULTS
-	char msg[512];
-	int len;
-	len = dr_snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-		"Instrumentation results: %llu instructions executed\n",
-		global_count);
-	DR_ASSERT(len > 0);
-	NULL_TERMINATE(msg);
-	DISPLAY_STRING(msg);
-#endif /* SHOW_RESULTS */
-	drmgr_exit();
 }
 
 static dr_emit_flags_t
@@ -202,4 +229,74 @@ bool for_trace, bool translating, void *user_data)
 		(void *)inscount, false /* save fpstate */, 1,
 		OPND_CREATE_INT32(num_instrs));
 	return DR_EMIT_DEFAULT;
+}
+
+
+static void
+event_exit(void)
+{
+#ifdef SHOW_RESULTS
+	char msg[512];
+	int len;
+	len = dr_snprintf(msg, sizeof(msg) / sizeof(msg[0]),
+		"Instrumentation results: %llu instructions executed\n",
+		global_count);
+	DR_ASSERT(len > 0);
+	NULL_TERMINATE(msg);
+	DISPLAY_STRING(msg);
+#endif /* SHOW_RESULTS */
+	drmgr_exit();
+}
+
+static void doCommandLineArgProcessing(client_id_t id){
+
+	const char * args = dr_get_options(id);
+
+	int i = 0;
+
+	int string_index = 0;
+	int index = -1;
+	int name_collect_state = 0;
+	int arguments_collect_state = 0;
+
+	while (args[i] != '\0'){
+		if (args[i] == '-'){
+			name_collect_state = 1;
+			arguments_collect_state = 0;
+			if (index >= 0){
+				arguments[index].arguments[string_index - 1] = '\0';
+			}
+			index++;
+			string_index = 0;
+			i++;
+			continue;
+		}
+
+		if (args[i] == ' ' && name_collect_state){
+			arguments[index].name[string_index++] = '\0';
+			name_collect_state = 0;
+			arguments_collect_state = 1;
+			string_index = 0;
+			i++;
+			continue;
+		}
+
+
+		if (name_collect_state){
+			arguments[index].name[string_index++] = args[i];
+		}
+		if (arguments_collect_state){
+			arguments[index].arguments[string_index++] = args[i];
+		}
+
+		i++;
+	}
+
+	//epilog
+	arguments[index].arguments[string_index++] = '\0';
+	argument_length = index + 1;
+
+
+	process_global_arguments();
+
 }
